ad85115b975d30ae5cf1a27d8e8ced15db6940bb
Mathew Taylor <mat@mathewtaylor.co.uk>
2023-12-02 17:00:55 +0000

    about to remove reversal

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -4,20 +4,65 @@ import (
 	"log"
 
 	"github.com/mathew/advent-of-code-2023/cmd/day1/trebuchet"
+	"github.com/mathew/advent-of-code-2023/internal/structures"
 )
 
 func main() {
+	partOne()
+	partTwo()
+}
 
+func partOne() {
 	results := []int{}
-
+	dm := &trebuchet.DigitMatcher{}
 	for _, in := range INPUT {
-		dm := trebuchet.DigitMatcher{}
-
 		_, f := trebuchet.First(in, dm)
 		_, l := trebuchet.Last(in, dm)
 
-		results = append(results, trebuchet.ConvertStrToInt(trebuchet.CombineRunes(f, l)))
+		results = append(results, trebuchet.ConvertStrToInt(string(f)+string(l)))
 	}
 
 	log.Printf("Day 1, part 1 result: %d", trebuchet.Sum(results...))
+}
+
+func partTwo() {
+	wordsMap := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	words := structures.Keys(wordsMap)
+
+	ins := []string{
+		"threexyzthreet",
+	}
+
+	dm := &trebuchet.DigitMatcher{}
+	wm := trebuchet.NewWordMatcher(words)
+	wc := trebuchet.NewWordConverter(wordsMap, wm)
+	fwodm := trebuchet.NewMultiMatcher(wc, dm)
+
+	rwm := trebuchet.NewReverseWordMatcher(words)
+	rwc := trebuchet.NewWordConverter(wordsMap, rwm)
+	rfwodm := trebuchet.NewMultiMatcher(rwc, dm)
+
+	results := []int{}
+	for _, in := range ins {
+		_, f := trebuchet.First(in, fwodm)
+		_, l := trebuchet.Last(in, rfwodm)
+
+		combined := string(f) + string(l)
+
+		println("combined")
+		println(combined)
+		results = append(results, trebuchet.ConvertStrToInt(combined))
+	}
+
+	log.Printf("Day 1, part 2 result: %d", trebuchet.Sum(results...))
 }
--- cmd/day1/trebuchet/trebuchet.go
+++ cmd/day1/trebuchet/trebuchet.go
@@ -1,53 +1,146 @@
 package trebuchet
 
 import (
+	"log"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 type Matcher interface {
-	Matches(rune) bool
+	Matches(rune) (bool, string)
 }
 
 type DigitMatcher struct{}
 
-func (dm DigitMatcher) Matches(r rune) bool {
-	return unicode.IsDigit(r)
+func NewDigitMatcher() Matcher {
+	return DigitMatcher{}
 }
 
-func First(str string, matcher Matcher) (bool, rune) {
+func (dm DigitMatcher) Matches(r rune) (bool, string) {
+	return unicode.IsDigit(r), string(r)
+}
+
+type WordConverter struct {
+	wordsMapping map[string]string
+	matcher      Matcher
+}
+
+func NewWordConverter(wordsMapping map[string]string, matcher Matcher) Matcher {
+	return WordConverter{wordsMapping, matcher}
+}
+
+func (wm WordConverter) Matches(r rune) (bool, string) {
+	ok, val := wm.matcher.Matches(r)
+	if ok {
+		val = wm.wordsMapping[val]
+	}
+
+	return ok, val
+}
+
+type WordMatcher struct {
+	context string
+	words   []string
+	reverse bool
+}
+
+func NewWordMatcher(words []string) Matcher {
+	wm := WordMatcher{"", words, false}
+	return &wm
+}
+
+func NewReverseWordMatcher(words []string) Matcher {
+	wm := WordMatcher{"", words, true}
+	return &wm
+
+}
+
+func (wm *WordMatcher) Matches(r rune) (bool, string) {
+	if unicode.IsDigit(r) {
+		wm.context = ""
+		return false, ""
+	}
+
+	if wm.reverse {
+		wm.context = string(r) + wm.context
+	} else {
+		wm.context = wm.context + string(r)
+	}
+
+	for _, w := range wm.words {
+		if w == wm.context {
+			match := wm.context
+			wm.context = ""
+			return true, match
+		}
+
+		if strings.Contains(w, wm.context) {
+			return false, ""
+		}
+	}
+	wm.context = ""
+	return false, ""
+}
+
+type MultiMatcher struct {
+	words  Matcher
+	digits Matcher
+}
+
+func NewMultiMatcher(words Matcher, digits Matcher) Matcher {
+	mm := MultiMatcher{words, digits}
+	return &mm
+}
+
+func (fwodm *MultiMatcher) Matches(r rune) (bool, string) {
+	ok, val := fwodm.words.Matches(r)
+	if ok {
+		return true, val
+	}
+
+	ok, val = fwodm.digits.Matches(r)
+	if ok {
+		return true, val
+	}
+
+	return false, ""
+}
+
+func First(str string, matcher Matcher) (bool, string) {
 	rs := []rune(str)
 
 	l := len(rs)
 	for x := 0; x < l; x++ {
-		if matcher.Matches(rs[x]) {
-			return true, rs[x]
+		ok, val := matcher.Matches(rs[x])
+		if ok {
+			return true, val
 		}
 	}
 
-	return false, rune(0)
+	return false, ""
 }
 
-func Last(str string, matcher Matcher) (bool, rune) {
+func Last(str string, matcher Matcher) (bool, string) {
 	rs := []rune(str)
 
 	l := len(rs)
 	for x := l - 1; x >= 0; x-- {
-		if matcher.Matches(rs[x]) {
-			return true, rs[x]
+		ok, val := matcher.Matches(rs[x])
+		if ok {
+			return true, val
 		}
 	}
 
-	return false, rune(0)
-}
-
-func CombineRunes(a, b rune) string {
-	return string([]rune{a, b})
+	return false, ""
 }
 
 func ConvertStrToInt(str string) int {
+	println("converting:")
+	println(str)
 	i, err := strconv.Atoi(str)
 	if err != nil {
+		log.Printf("%v", err)
 		panic("trebuchet: String to Int Conversion")
 	}
 
