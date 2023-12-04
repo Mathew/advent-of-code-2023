87198073cd3b8f398e93fc2ea952b6d4501a1a65
Mathew Taylor <mat@mathewtaylor.co.uk>
2023-12-04 20:37:20 +0000

    day 4, part 2

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -7,12 +7,26 @@ import (
 
 func main() {
 	partOne()
+	partTwo()
 }
 
 type Game struct {
 	id      int
 	results []int
 	card    []int
+}
+
+func (g Game) Matches() []int {
+	matches := []int{}
+
+	for _, r := range g.results {
+		if slices.Contains(g.card, r) {
+			matches = append(matches, r)
+		}
+	}
+
+	return matches
+
 }
 
 func Scorer() func() int {
@@ -29,6 +43,21 @@ func Scorer() func() int {
 	}
 }
 
+func xrange(start, end, limit int) []int {
+	arr := []int{}
+
+	for i := start; i < end+1; i++ {
+		if i > limit {
+			break
+		}
+
+		arr = append(arr, i)
+	}
+
+	return arr
+
+}
+
 func partOne() {
 	TotalScore := 0
 
@@ -36,14 +65,31 @@ func partOne() {
 		scorer := Scorer()
 		finalScore := 0
 
-		for _, r := range game.results {
-			if slices.Contains(game.card, r) {
-				finalScore = scorer()
-			}
+		for range game.Matches() {
+			finalScore = scorer()
 		}
 
 		TotalScore += finalScore
 	}
 
 	log.Printf("Day 4, part 1: %v", TotalScore)
+}
+
+func partTwo() {
+	scratchCardMap := map[int]int{}
+	scratchCardCount := map[int]int{}
+	total := 0
+
+	for _, game := range INPUT {
+		scratchCardMap[game.id] = len(game.Matches())
+		scratchCardCount[game.id] += 1
+
+		total += scratchCardCount[game.id]
+
+		for _, id := range xrange(game.id+1, game.id+scratchCardMap[game.id], 207) {
+			scratchCardCount[id] += 1 * scratchCardCount[game.id]
+		}
+	}
+
+	log.Printf("Day 4, part 2: %v", total)
 }
